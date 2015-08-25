064d654ec9cdbab6ae4d02ff821fcd68e332048c
Chris McCammon <cmccammon@gmail.com>
2015-08-25 11:57:20 -0500

    double and square added plus loop

---
+++ methods.rb
@@ -0,0 +1,44 @@
+a = 10
+b = 12
+
+def multiply(first, last)# argument list for method
+  first * last #function body
+end
+
+def double(x)
+  x * 2
+end
+
+def square(x)
+  x * x
+end
+#arity; (number of arguments) has to match method
+
+puts multiply(10, 20)
+
+system('clear') # clear is command line. system tells it to run
+loop do
+    puts "Would you like to do (D)ouble or(S)quare a number or (Q)uit?"
+    print "D or S or Q? >" #three option
+
+    response = gets.chomp.upcase
+    if response == "D"
+      print "what number do you want to double?"
+      response_number = gets.chomp.to_i
+      puts "Double of #{response_number} is #{double(response_number)}"
+    elsif response == "S"
+      print "what number do you want to square?"
+      response_number = gets.chomp.to_i
+      puts "Square of #{response_number} is #{square(response_number)}"
+    elsif response == "Q"
+      puts "goodbye"
+      exit
+    else
+      puts "what?"
+    end#end of if
+  end#end of loop
+
+
+
+
+
