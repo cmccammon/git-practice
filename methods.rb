afda18e6b1c62c82a7a0724f91ce93407efcf3da
Chris McCammon <cmccammon@gmail.com>
2015-08-25 11:59:50 -0500

    DRY

--- methods.rb
+++ methods.rb
@@ -18,25 +18,24 @@ puts multiply(10, 20)
 
 system('clear') # clear is command line. system tells it to run
 loop do
-    puts "Would you like to do (D)ouble or(S)quare a number or (Q)uit?"
-    print "D or S or Q? >" #three option
-
-    response = gets.chomp.upcase
-    if response == "D"
-      print "what number do you want to double?"
-      response_number = gets.chomp.to_i
-      puts "Double of #{response_number} is #{double(response_number)}"
-    elsif response == "S"
-      print "what number do you want to square?"
-      response_number = gets.chomp.to_i
-      puts "Square of #{response_number} is #{square(response_number)}"
-    elsif response == "Q"
-      puts "goodbye"
-      exit
-    else
-      puts "what?"
-    end#end of if
-  end#end of loop
+  puts "Would you like to do (D)ouble or(S)quare a number or (Q)uit?"
+  print "D or S or Q? >" #three options
+  response = gets.chomp.upcase #error correction upcase
+  if response == "D"
+    print "what number do you want to double?"
+    response_number = gets.chomp.to_i
+    puts "Double of #{response_number} is #{double(response_number)}"
+  elsif response == "S"
+    print "what number do you want to square?"
+    response_number = gets.chomp.to_i
+    puts "Square of #{response_number} is #{square(response_number)}"
+  elsif response == "Q"
+    puts "goodbye"
+    exit
+  else
+    puts "what?"
+  end#end of if
+end#end of loop
 
 
 
