f2ded0aee4f0782430e300d38c18cafc4b36fc2e
Chris McCammon <cmccammon@gmail.com>
2015-08-25 18:58:46 -0500

    fixed loop end

--- bouncer.rb
+++ bouncer.rb
@@ -7,20 +7,20 @@ system ('clear')
   def check_age()
     loop do
       print "How old are you? or q to quit>"#ask the user their age
-      response_age = gets.chomp.to_i
-      if response_age <= 18 && response_age >= 1
+      response_age = gets.chomp.downcase #removed the to_i and moved it to each responce so q would work right.
+      if response_age.to_i <= 18 && response_age.to_i >= 1
         puts "Sorry you can't do anything fun"
       end
-      if response_age >= 18 #&& response_age <= 20
+      if response_age.to_i >= 18 #&& response_age <= 20
         puts "You can vote and smoke(but really, don't smoke)!"
       end
-      if response_age >= 21 #&& response_age <= 24
+      if response_age.to_i >= 21 #&& response_age <= 24
         puts "You can enjoy an alcoholic beverage"
       end
-      if response_age >= 25
+      if response_age.to_i >= 25
         puts "You can rent a car too!"
       end
-      if response_age == 0
+      if response_age == "q"
       # puts "Would you like to quit? yes/no >"
       # quit = gets.chomp.downcase
       # if quite == "yes"
